8491cd4325d434cce6bc7bdde477fbde5cbdcb88
Paula <paulasalinor@gmail.com>
2022-06-07 19:26:46 -0300

    feat context directory

---
+++ src/context/ProdutcsProvider.tsx
@@ -0,0 +1,26 @@
+import {ProductsContext} from './ProdutcsContext';
+import React, {useState, ReactNode, useContext} from 'react';
+
+interface Props {
+  code: string,
+  category: string,
+  name_product: string,
+  name_provider: string,
+  price: string,
+}
+
+export const ProductsProvider = ({children}: ReactNode) => {
+  const [product, setProduct] = useState<Props[]>([]);
+  const data = {
+    product,
+    setProduct
+  }
+
+  return (
+    <ProductsContext.Provider value={data}>
+      {children}
+    </ProductsContext.Provider>
+  )
+}
+
+export const useProducts = () => useContext(ProductsContext);
