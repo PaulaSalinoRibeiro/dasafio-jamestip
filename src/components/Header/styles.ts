5679e00e9a04e3da09bf8f8e88a2b8fcb63466a8
Paula <paulasalinor@gmail.com>
2022-06-07 23:44:11 -0300

    applied style in Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,4 +3,19 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background-color: ${({theme}) => theme.colors.primary};
   padding: 20px 0;
+  width: 100%;
+
+  nav {
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    width: 30%;
+
+    a {
+      text-decoration: none;
+      color: ${({theme}) => theme.colors.shape};
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
 `;
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     background: '#F0F2F5';
+    font-family: 'Poppins', sans-serif;
   }
 `
 
